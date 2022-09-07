8717b940135724bd14ed0d0441c8799e70f34ed7
Habibu Haruna Durumi <89111257+habibu-HD@users.noreply.github.com>
2022-09-07 09:39:30 +0100

    Update About.js
    made changes to symbols

--- components/About.js
+++ components/About.js
@@ -10,17 +10,17 @@ const About = () => {
         <div className='bg-green-800  p-6 rounded-md'>
           <p className='text-center leading-10 text-white text-opacity-50 tracking-wide font-normal  md:text-lg md:text-left'>
             A microbiologist who changed track to become a web developer by
-            dedicating <span>3+ years</span> of my career to web development and
+            dedicating 3+ years of my career to web development and
             Search Engine Optimization (SEO), building and deploying responsive
-            and bug-free frontend website, I feel incredibly connected to web
+            and bugfree frontend website, I feel incredibly connected to web
             develoment goal of transforming the programming technology industry
             through developing and growing meaningful websites globally. <br />
             <span className='block my-3'>
               As a self-taught frontend web junkie, when I visit a website or
               click and surf into web apps, I immediately start analyzing
               everything from the User Experience(UX) and the user Interface
-              (UI), to the source code, operations and functionality - and
-              that&apos;s just in my spare time!
+              (UI), to the source code, operations and functionality  and
+              that is just in my spare time!
             </span>
             <span>
               Being a Nigerian, I have grown up with curiousity for programming
